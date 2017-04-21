69646e0497413002db57f89dc45e04934bef0907
nguyenkimbinh <hoangnguyen291@hotmail.com>
2017-04-21 16:03:46 +0700

    initial

---
+++ src/MultiAPI.SPAWeb/webpack.config.js
@@ -0,0 +1,17 @@
+﻿// Look in ./config folder for webpack.dev.js
+
+"use strict";
+
+var fs = require('fs');
+var gracefulFs = require('graceful-fs');
+gracefulFs.gracefulify(fs);
+switch (process.env.NODE_ENV) {
+    case 'prod':
+    case 'production':
+        module.exports = require('./config/webpack.prod')({ env: 'production' });
+        break;
+    case 'dev':
+    case 'development':
+    default:
+        module.exports = require('./config/webpack.dev')({ env: 'development' });
+}
